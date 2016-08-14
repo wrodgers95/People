ae122c77e2f2820f3e3f20081472972a26b8cffb
wrodgers95 <will.rodgers1995@gmail.com>
2016-08-14 13:08:08 -0400

    Saving... Not working

--- src/com/theironyard/charlotte/Main.java
+++ src/com/theironyard/charlotte/Main.java
@@ -1,7 +1,34 @@
 package com.theironyard.charlotte;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        HashMap<String, ArrayList<Person>> People = new HashMap<>();
+
+        File f = new File ("people.csv");
+
+        Scanner scanner = new Scanner(f);
+        while (scanner.hasNext()){
+
+            String line = scanner.nextLine();
+            String[] columns = line.split(",");
+            Person peopleColumns = new Person(columns[0], columns[1], columns[2], columns[3], columns[4], columns[5]);
+            People.putIfAbsent(columns[4], new ArrayList<>());
+            People.get(columns[4]).add(peopleColumns);
+            Collections.sort(People.get(columns[4]));
+            System.out.println(People);
+
+
+
+
+        }
     }
 }
---
+++ src/com/theironyard/charlotte/Person.java
@@ -0,0 +1,75 @@
+package com.theironyard.charlotte;
+
+
+class Person implements Comparable {
+
+    Person (String firstName, String lastName, String country, String email, String ip, String id) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.country = country;
+        this.email = email;
+        this.ip = ip;
+        this.id = id;
+    }
+
+    private String firstName;
+    private String lastName;
+    private String country;
+    private String email;
+    private String ip;
+    private String id;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        Person p = (Person) o;
+        return Integer.parseInt(p.lastName);
+    }
+}
